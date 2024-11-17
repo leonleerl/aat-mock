d22eef8f8bd6baa52a8a22770201e311911a855b
leonleerl <leon.runlongli@gmail.com>
2024-11-17 16:28:17 +0800

    init

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,20 @@
+
+
+const Home = () => {
+    
+    return <>
+    <div className=" container">
+        <div className=" bg-[#052A36] flex">
+            <div className=" text-white ml-10">Home</div>
+            <div className=" text-white">Daily listings</div>
+            <div className=" text-white">Language help</div>
+            <div className=" text-white">Contact us</div>
+            <div className=" text-white">Search</div>
+
+        </div>
+    </div>
+        
+    </>
+}
+
+export default Home;
